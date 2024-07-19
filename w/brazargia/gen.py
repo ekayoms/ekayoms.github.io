7699a1e7780c8cd7d89f613343870733d3091d99
EpicRaisin <65032841+LameGrape@users.noreply.github.com>
2024-07-18 19:10:36 -0500

    14

--- w/brazargia/gen.py
+++ w/brazargia/gen.py
@@ -1,24 +1,16 @@
-text = """The conversation trailed off. The migraine persisted, and it would probably be a while until the painkiller took effect. And considering the sheer amount of pain, I seriously considered the possibility of asking for another dose later. But for now, what I really needed was just to take a breather. Too much had happened and too long a distance had been walked--not to mention stairs climbed--so I continued leaning against the wall with my arms crossed. I was still staring at the only even remotely interesting thing in that room, Vac, with a slight, smug smile, which I was largely forcing to hide the chaos in my mind. He was staring back, more or less, although he randomly moved and looked around the room.
-Eventually, I broke the ice:
-“So... technically, I am discharged from the hospital, right?”
-“I don’t have the expertise to diagnose you, ma’am, but I suppose that if you’re feeling well enough to be wandering around, sometimes armed with dangerous objects... although it would be prudent to perform proper exams on you.”
-Fat chance. I never wanted to see another needle again after the IV ordeal. My arm was also still hurting from that.
-“I’ll pass, thank you very much. In any case... what if I were to, you know... leave?”
-“Leave? You can leave at any time, ma’am. The exit is just across the garden from this kitchen.”
-Hold on. The other set of double doors... I hadn’t checked them at all. I pretty much instantly went running towards it, shouting as I went:
-“You coming or what?!”
-“Ma’am, I’m still not done with my cleanup on the sec-”
-“Screw that! Command override! Follow me!”
-“Fine, ma’am, wait for me!” I could hear his wheels setting off.
-The garden was too overgrown to directly cross, so I went by the corridors again. I could now notice that the sunrays from above were different: they were distinctly more red. As I passed the door to the patient rooms, I had an interesting thought: if I had taken the right instead of the left when I arrived at the ground floor, I’d already be out in the open world again, probably. But then I would never have met Vac. The extra time spent there had been worth it. The idea of continuing alone sounded... depressing, now.
-Especially because of what happened after.
-I reached the doors--panting, of course--and waited for Vac to arrive, disappointedly staring at the “EXIT” sign above the doors, wondering how I had missed that. When I looked at him, I noticed his wheels had left track marks on the floor. Good thing he wouldn’t have to clean that anymore--he just didn’t know it. I didn’t want to tell him he was coming with me yet.
-Yes, he was just an algorithm, but I still didn’t feel like... shocking him like that, considering tending to the hospital had been his entire purpose so far, mostly. My idea was just to make him stick to me for as long as possible and see what would happen. I supposed that at some point he’d ask where we were going. I didn’t intend to answer him. Not because I didn’t want to, but because I had no idea, either.
-At any rate, I opened the doors. We were faced with a hallway similar to the ones with the bedrooms, but it felt even more dimly lit. Most lighting, in fact, came from the “EXIT” sign at the end of it--it glowed red, instead of the usual green. And there were no doors on either side. It was an ominous sight. Figuring nothing could really go wrong, I said:
-“Well, ladies first.” I felt it was an appropriate idiom, somehow, despite being fairly sure it wasn’t used in contexts like crossing a dark corridor. I started slowly walking forward, and Vac wheeled behind me. A bit loudly, too. In spite of the eeriness of the scene, however, I can assure you it wasn’t the most terrifying part.
-The most terrifying part was when I realized the door was made of steel, pressed the button to open it, and was answered by an actual robotic voice coming from the panel’s speakers:
-“ACCESS DENIED. SECTOR UNDER LOCKDOWN FOR... 1... 2... 4... 1... DAYS DUE TO... LEVEL... 4... BIOHAZARD THREAT.”
-My world stopped after the word “lockdown”. As the voice continued, my face slowly changed to the most shocked expression you can possibly imagine. When the announcement ended, I don’t think any of my moments since waking up so far had been so silent. Vac was quiet; his screen also had a surprised expression. We looked at each other. Another idiom came to me. Even as an amnesiac, I was certain it was the only one that could possibly convey my feelings:
-“What the FUCK?!”"""
+text = """Everything was spinning. Almost as a reflex, I shook my head, took a deep breath, and pressed the button again. Surely that had only been an error. It was the first time in forever that someone had pressed it. It was broken. I wasn’t stuck there. I couldn’t be. It simply wasn’t possible. It wasn’t conceivable that I had suffered an accident, lost a leg, fallen into a coma for years, woken up to find myself alone and unable to remember anything, and was trapped there-
+“ACCESS DENIED. SECTOR UNDER LOCKDOWN FOR--”
+I screamed and punched the door. It hurt a lot, but I closed my eyes and did it again. And again. I only stopped when I heard a weak “ma’am” coming from behind. I snorted, frustrated, and turned around, my eyes wide open again--to the maximum extent they could be. I was shaking. Vac was too--his wheels were dancing randomly on the floor--but I had the impression that was more because he was scared of me.
+“M-ma’am,” he said again, “please... don’t hurt yourself like this...”
+I looked at my hand. It was red, and commenting on the pain is unnecessary, but I could barely feel it through the adrenaline. None of that felt real. I continued staring down at Vac as I tried to process what I had just heard. Futile. There was nothing to process--the message had already come across. A lot of words had been said by the speaker, and not all of them made sense to me, but that didn’t matter. The way I interpreted them was crystal clear:
+“You will never know who you are. You are just a number, Patient 427.”
+“You will never see another person again. You better hope that robot outlives you.”
+“Those orchids aren’t yours--they are for your grave.”
+As these thoughts echoed in my head, a smile spontaneously came across my face. And then my nose started blowing air out. It wasn’t long until the cycle became self-sustaining. When I woke up, I found something as innocent as my mattress being marked by my extended permanence on it funny. But this...? This was the pinnacle of comedy: the story of the most unfortunate woman who had ever lived. I couldn’t stop laughing.
+“Ma’am...!” Vac said after a solid minute of my crisis, probably panicking at the sight. “C-calm down... I know this is hard for you, but you clearly aren’t-”
+“Aren’t okay? Yeah, I’m not!" I interrupted him as I laughed. “Of course I’m not okay! Don’t you understand? I’m... hah... I’m... hahaha... I’m going to DIE IN HERE! ALONE! WITH A REALLY NICE TALKING VACUUM CLEANER! ISN’T IT HILARIOUS?!”
+“N-not really, no...” He replied, his expression worried. “Listen... you should sit down and...”
+At this point, everything becomes hazy. Dreamy. I can’t really tell what happened. I probably started ignoring him and wandering off. Where to is unclear. Even more unclear is what I wanted to achieve. I do have flashes of opening the door back into the lobby, and seeing the “1” painted on the wall by the stairs, going up on them, and the elevator doors opening--but not much beyond that. One feeling, however, I can distinctly remember:
+The electrical shock of 1200 volts from behind, causing me to pass out."""
 
 open("gen.txt", "w+", encoding="utf-8").write("\n".join([f"<p>{a.strip()}</p>" for a in text.split("\n")]))
